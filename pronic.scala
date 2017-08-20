b7ee8677c3f63388c07099c155e801385e301e9d
drm <drm@localhost.localdomain>
2017-08-20 17:35:03 -0400

    add tail rec

--- pronic.scala
+++ pronic.scala
@@ -1,5 +1,7 @@
 def pronic(i: Int, j: Int): List[Int] = {
     if (i < 0 || j < 0) throw new Exception("All values must be greater than zero.")
+    import scala.annotation.tailrec
+    @tailrec
     def pronicR(s: Int, e: Int, res: List[Int]): List[Int] = {
         if (e < s) res
         else pronicR(s, e - 1, (e * (e - 1)) :: res)
